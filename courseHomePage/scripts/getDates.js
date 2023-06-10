700a29bb35887668d4406eea113337ee5f7399b7
Nilber Mota <nilber16@outlook.com>
2023-06-10 10:49:55 -0300

    feat: week 1 activity

---
+++ courseHomePage/scripts/getDates.js
@@ -0,0 +1,5 @@
+const currentDate = new Date();
+
+document.querySelector("#currentYear").innerHTML = currentDate.getFullYear();
+
+document.querySelector("#lastModified").innerHTML = document.lastModified;
