535758a8e8104625d06e49f151bba10996c4f4d4
Nilber Mota <nilber16@outlook.com>
2023-06-17 09:32:07 -0300

    feat: added dark mode to course page

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,15 @@ const menuList = document.querySelector("#menuList");
 const imageBtnOpen = document.querySelector("#imageBtnOpen");
 const imageBtnClose = document.querySelector("#imageBtnClose");
 
+const darkBtn = document.querySelector("#darkBtn");
+const body = document.querySelector("body");
+
 document.getElementById("menuBtn").addEventListener("click", () => {
   menuList.classList.toggle("menuClose");
   imageBtnOpen.classList.toggle("visible");
   imageBtnClose.classList.toggle("visible");
+});
+
+darkBtn.addEventListener("click", () => {
+  body.classList.toggle("darkMode");
 });
